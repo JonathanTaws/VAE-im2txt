5bd9efa7406469360e90129f2229dbe386d96295
JonathanTaws <jonathan.taws@gmail.com>
2016-11-07 17:08:53 +0100

    New method to keep list of images and corresponding labels - to finish

--- download_images.py
+++ download_images.py
@@ -1,4 +1,6 @@
 import os, urllib
+from collections import defaultdict
+
 from pycocotools.coco import COCO
 
 MSCOCO_URL = 'http://mscoco.org/images/'
@@ -9,10 +11,10 @@ data_type = 'train2014'
 ann_file = '%s/annotations/instances_%s.json' % (data_dir, data_type)
 
 # Define 4 labels we'll work with
-labels = ['umbrella']
+labels = {'elephant': 0, 'person': 1, 'skis': 2, 'toilet': 3}
 
 # Number of images to download per label
-NB_IMG = 10
+NB_IMG = 100
 
 def init_COCO():
     # initialize COCO api for captioning
@@ -33,6 +35,27 @@ def download_images(coco):
             if os.path.exists(filename):
                 continue
             # Download the image and place it in the file
+
+            urllib.urlretrieve(MSCOCO_URL + '/%d' % img['id'], filename)
+
+# TODO Finish
+def get_images_filenames(coco):
+    images_labels = defaultdict([0] * len(labels))
+
+    for label, idx in labels.iteritems():
+        cat_ids = coco.getCatIds(catNms=label)
+        img_ids = coco.getImgIds(catIds=cat_ids)
+        imgs = coco.loadImgs(img_ids[:NB_IMG])
+
+        for img in imgs:
+            filename = img['file_name']
+
+            # TODO Finsih
+            full_path = ''
+            if os.path.exists(full_path):
+                images_labels[full_path].append(idx)
+            # Download the image and place it in the file
+
             urllib.urlretrieve(MSCOCO_URL + '/%d' % img['id'], filename)
 
 if __name__ == "__main__":
